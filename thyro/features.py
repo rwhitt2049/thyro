79bebd3bdd36f2e3346b3bdd541a838774c6f511
rwhitt2049 <rwhitt2049@gmail.com>
2017-04-09 22:32:09 -0500

    Change all references of operations to statistics in features.py
    Better representation of functionality

--- thyro/features.py
+++ thyro/features.py
@@ -6,25 +6,25 @@ import numpy as np
 __all__ = ['Feature', 'create_feature', 'BaseFeature']
 
 
-FACTORY = {
+STATISTIC_FACTORY = {
     'min': np.min,
     'max': np.max,
     'mean': np.mean,
 }
 
 
-def operation_factory(operation):
-    if isinstance(operation, str):
+def statistic_factory(statistic):
+    if isinstance(statistic, str):
         try:
-            _operation = FACTORY[operation]
+            _statistic = STATISTIC_FACTORY[statistic]
         except KeyError:
-            raise NotImplementedError('%s is not implemented' % operation)
-    elif isinstance(operation, Callable):
-        _operation = operation
+            raise NotImplementedError('%s is not implemented' % statistic)
+    elif isinstance(statistic, Callable):
+        _statistic = statistic
     else:
-        raise TypeError('%s must be a callable or a string name for a default operation.' % operation)
+        raise TypeError('%s must be a callable or a string name for a default statistic.' % statistic)
 
-    return _operation
+    return _statistic
 
 
 class BaseFeature(metaclass=ABCMeta):
@@ -34,10 +34,10 @@ class BaseFeature(metaclass=ABCMeta):
 
 
 class Feature(BaseFeature):
-    def __init__(self, data, operation, name, is_nominal=False):
+    def __init__(self, data, statistic, name, is_nominal=False):
         self.data = data
         self.name = name
-        self._operation = operation
+        self._statistic = statistic
         if isinstance(is_nominal, bool):
             self.is_nominal = is_nominal
         else:
@@ -46,14 +46,14 @@ class Feature(BaseFeature):
     @property
     def operation(self):
         # TODO cache
-        return operation_factory(self._operation)
+        return statistic_factory(self._statistic)
 
     def __call__(self, item=slice(None, None)):
         return self.operation(self.data[item])
 
 
-def create_feature(data, operation):
-    _operation = operation_factory(operation)
+def create_feature(data, statistic):
+    _operation = statistic_factory(statistic)
 
     def _call(item=slice(None, None)):
         return _operation(data[item])
