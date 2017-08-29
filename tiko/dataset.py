3e9897172631f4e8709c4cfb1c0bc57e5bfc1b7b
rwhitt2049 <rwhitt2049@gmail.com>
2017-08-29 00:44:06 -0500

    Add __bool__ to dataset, remove concat function

--- tiko/dataset.py
+++ tiko/dataset.py
@@ -1,9 +1,8 @@
 import abc
 
-import numpy as np
 import pandas as pd
 
-__all__ = ['extract_features', 'concat']
+__all__ = ['extract_features']
 
 
 def gen_feature_space(features, segments):
@@ -26,8 +25,7 @@ def extract_features(features, segments=None, labels=None):
     if labels:
         return LabeledDataSet(feature_names, labels, categorical_features, df)
     else:
-        # return UnlabeledDataSet(feature_space, categorical_features)
-        raise NotImplemented('currently only unlabeled data supported')
+        return UnlabeledDataSet(feature_space, categorical_features)
 
 
 class DataSet(abc.ABC):
@@ -53,6 +51,9 @@ class DataSet(abc.ABC):
     def __dir__(self):
         return super().__dir__() + dir(self._data)
 
+    def __bool__(self):
+        return bool(len(self._data))
+
 
 class UnlabeledDataSet(DataSet):
     def __init__(self, *args, **kwargs):
@@ -63,17 +64,3 @@ class LabeledDataSet(DataSet):
     def __init__(self, labels, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.labels = labels
-
-
-def concat(*datasets):
-    # check all same type
-    # check categorical features are the same
-    # call pd.concat
-
-    if datasets:
-        categorical_features = datasets[0].categorical_features
-    else:
-        categorical_features = []
-
-    df = pd.concat(datasets)
-    return UnlabeledDataSet(df, categorical_features)
